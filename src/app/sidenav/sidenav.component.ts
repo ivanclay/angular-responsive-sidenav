16406f8792be1b8de3c50ef3e1ce5f6404ac0eab
ivanclay <ivan.clay.moura@gmail.com>
2022-03-26 09:55:28 -0300

    Ajustes de layout

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -11,32 +11,32 @@ interface SideNavToggle {
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
-  // animations: [
-  //   trigger('fadeInOut', [
-  //     transition('enter', [
-  //       style({opacity: 0}),
-  //       animate('350ms',
-  //         style({opacity: 1})
-  //       )
-  //     ]),
-  //     transition('enter', [
-  //       style({opacity: 1}),
-  //       animate('350ms',
-  //         style({opacity: 0})
-  //       )
-  //     ])
-  //   ]),
-  //   trigger('rotate', [
-  //     transition('enter', [
-  //       animate('1000ms',
-  //       keyframes([
-  //         style({transform: 'rotate(0deg)', offset: '0'}),
-  //         style({transform: 'rotate(2turn)', offset: '1'})
-  //       ])
-  //       )
-  //     ])
-  //   ])
-  // ]
+   animations: [
+     trigger('fadeInOut', [
+       transition(':enter', [
+         style({opacity: 0}),
+         animate('350ms',
+           style({opacity: 1})
+         )
+       ]),
+       transition(':leave', [
+         style({opacity: 1}),
+         animate('350ms',
+           style({opacity: 0})
+         )
+       ])
+     ]),
+     trigger('rotate', [
+       transition(':enter', [
+         animate('1000ms',
+         keyframes([
+           style({transform: 'rotate(0deg)', offset: '0'}),
+           style({transform: 'rotate(2turn)', offset: '1'})
+         ])
+         )
+       ])
+     ])
+   ]
 })
 
 export class SidenavComponent implements OnInit {
